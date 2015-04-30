4cb37de5eda012998e4d6096992fcbceab44670d
dmadathil <dmadathil@DILEEPMADATHIL.local>
2015-04-30 13:13:57 -0400

    first commit

---
+++ actio-parent/server/src/main/java/com/covisint/action/server/service/ActioController.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.covisint.action.server.service;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.covisint.core.http.service.server.Version;
+import com.covisint.core.http.service.server.view.ModelSupport;
+
+/**
+ * @author dmadathil
+ *
+ */
+@Controller
+@RequestMapping(value = "/actio")
+public class ActioController {
+	
+	private final Logger log = LoggerFactory.getLogger(ActioController.class);
+
+	/**
+	 * 
+	 */
+	public ActioController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	  @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	  @ResponseBody
+	    public String getById(@PathVariable String id) {
+
+	        log.debug("Attempting to retrieve resource with ID {}", id);
+
+	       // final Map<String, Object> responseModel = ModelSupport.createModel();
+	        log.debug("Retrieved the following resource with ID {}: {}", "DILEEP", "SUCCESS");
+            //ModelSupport.setVersion(responseModel, currentResourceVersion);
+           // ModelSupport.setBody(responseModel, "SUCCESS");
+            return "SUCCESS";
+	  }
+
+}
