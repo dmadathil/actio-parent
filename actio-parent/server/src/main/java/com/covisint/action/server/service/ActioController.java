adf8776c81e3681a0a5c9a5d417d4bc294eb3723
dwdamon <dwdamon@gmail.com>
2015-05-01 00:14:53 -0400

    echolib calls in place and functioning.

--- actio-parent/server/src/main/java/com/covisint/action/server/service/ActioController.java
+++ actio-parent/server/src/main/java/com/covisint/action/server/service/ActioController.java
@@ -3,23 +3,18 @@
  */
 package com.covisint.action.server.service;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.covisint.core.http.service.server.Version;
-import com.covisint.core.http.service.server.view.ModelSupport;
+import com.javasteam.amazon.echo.EchoUserSession;
 import com.javasteam.amazon.echo.plugin.Builtin;
+import com.javasteam.util.Configurator;
 
 /**
  * @author dmadathil
@@ -30,6 +25,14 @@ import com.javasteam.amazon.echo.plugin.Builtin;
 public class ActioController {
 	
 	private final Logger log = LoggerFactory.getLogger(ActioController.class);
+	
+	private static final EchoUserSession echoUserSession;
+	static {
+	  Configurator    configurator    = new Configuration();
+	  
+	  echoUserSession = new EchoUserSession( configurator );
+	  echoUserSession.startPollers();
+	}
 
 	/**
 	 * 
@@ -38,9 +41,9 @@ public class ActioController {
 		// TODO Auto-generated constructor stub
 	}
 	
-	  @RequestMapping(method = RequestMethod.GET, value = "/{id}" ,produces =  {"text/plain; charset=UTF-8"})
+	  @RequestMapping(method = RequestMethod.GET, value = "/test/{id}" ,produces =  {"text/plain; charset=UTF-8"})
 	  @ResponseBody
-	    public String getDevice(@PathVariable String id) {
+	    public String getDeviceTest(@PathVariable String id) {
 
 	        log.debug("Attempting to retrieve resource with ID {}", id);
 
@@ -63,9 +66,9 @@ public class ActioController {
 	      return new ModelAndView("index");   
 	     }
 	  
-	    @RequestMapping(method = RequestMethod.GET, value = "/new/{id}" ,produces =  {"text/plain; charset=UTF-8"})
+	    @RequestMapping(method = RequestMethod.GET, value = "/{id}" ,produces =  {"text/plain; charset=UTF-8"})
         @ResponseBody
-          public String getDeviceNew(@PathVariable String id) {
+          public String getDevice(@PathVariable String id) {
 
               log.debug("Attempting to retrieve resource with ID {}", id);
 
@@ -76,7 +79,7 @@ public class ActioController {
               
               
               String retval = Builtin.fetchQueuedItem( id );
-              return retval != null ? retval : "TURNONREDLIGHT";
+              return retval != null ? retval : "";
         }
 
 }
